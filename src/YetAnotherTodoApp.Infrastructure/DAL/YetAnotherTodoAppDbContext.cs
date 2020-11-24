06532cc242740e86b0d0c3151dab5e6372278b45
Damian Lubera <dmlubera@gmail.com>
2020-11-24 21:15:04 +0100

    Added new entities to dbcontext

--- src/YetAnotherTodoApp.Infrastructure/DAL/YetAnotherTodoAppDbContext.cs
+++ src/YetAnotherTodoApp.Infrastructure/DAL/YetAnotherTodoAppDbContext.cs
@@ -6,11 +6,14 @@ namespace YetAnotherTodoApp.Infrastructure.DAL
     public class YetAnotherTodoAppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<TodoList> TodoLists { get; set; }
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<Step> Steps { get; set; }
 
         public YetAnotherTodoAppDbContext(DbContextOptions<YetAnotherTodoAppDbContext> options)
             : base(options)
         {
-            
+
         }
     }
 }
