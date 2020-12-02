d275fde97779836bbd1845848d492b3156546d9e
Damian Lubera <dmlubera@gmail.com>
2020-12-02 22:36:07 +0100

    Restored custom exception handling

--- src/YetAnotherTodoApp.Api/Startup.cs
+++ src/YetAnotherTodoApp.Api/Startup.cs
@@ -38,9 +38,7 @@ namespace YetAnotherTodoApp.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //app.UseMiddleware<ExceptionHandlerMiddleware>();
-
-            app.UseDeveloperExceptionPage();
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
 
             app.AddSwaggerMiddleware(Configuration);
 
