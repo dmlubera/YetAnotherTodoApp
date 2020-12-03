c604c7b5a03f49875b4506f0e45979107437374d
Damian Lubera <dmlubera@gmail.com>
2020-12-03 21:16:20 +0100

    Added request models for auth

---
+++ src/YetAnotherTodoApp.Api/Models/AuthenticateUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace YetAnotherTodoApp.Api.Models
+{
+    public class AuthenticateUserRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
