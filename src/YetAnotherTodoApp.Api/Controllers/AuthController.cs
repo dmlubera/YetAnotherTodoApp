37922b7ce370ff8be03bf537b412f204fba7684f
Damian Lubera <dmlubera@gmail.com>
2021-03-01 21:28:43 +0100

    Moved auth methods to separate controller

---
+++ src/YetAnotherTodoApp.Api/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Threading.Tasks;
+using YetAnotherTodoApp.Api.Models;
+using YetAnotherTodoApp.Application.Commands;
+using YetAnotherTodoApp.Application.Commands.Models;
+using YetAnotherTodoApp.Infrastructure.Auth.Commands.Models;
+
+namespace YetAnotherTodoApp.Api.Controllers
+{
+    [Route("api/auth/")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IMemoryCache _cache;
+
+        public AuthController(ICommandDispatcher commandDispatcher, IMemoryCache cache)
+        {
+            _commandDispatcher = commandDispatcher;
+            _cache = cache;
+        }
+
+        [HttpPost("register")]
+        public async Task RegisterUserAsync([FromBody] RegisterUserRequest request)
+        {
+            var command = new RegisterUserCommand(request.Username, request.Email, request.Password);
+            await _commandDispatcher.DispatchAsync(command);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginUserAsync([FromBody] AuthenticateUserRequest request)
+        {
+            var command = new LoginUserCommand
+            {
+                TokenId = Guid.NewGuid(),
+                Email = request.Email,
+                Password = request.Password
+            };
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok(_cache.Get(command.TokenId));
+        }
+    }
+}
--- src/YetAnotherTodoApp.Api/Controllers/UserController.cs
+++ src/YetAnotherTodoApp.Api/Controllers/UserController.cs
@@ -2,14 +2,12 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
 using YetAnotherTodoApp.Api.Models;
 using YetAnotherTodoApp.Application.Commands;
 using YetAnotherTodoApp.Application.Commands.Models;
 using YetAnotherTodoApp.Application.DTOs;
 using YetAnotherTodoApp.Application.Queries;
 using YetAnotherTodoApp.Application.Queries.Models;
-using YetAnotherTodoApp.Infrastructure.Auth.Commands.Models;
 
 namespace YetAnotherTodoApp.Api.Controllers
 {
@@ -18,13 +16,11 @@ namespace YetAnotherTodoApp.Api.Controllers
     {
         private readonly ICommandDispatcher _commandDispatcher;
         private readonly IQueryDispatcher _queryDispatcher;
-        private readonly IMemoryCache _cache;
 
-        public UserController(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher, IMemoryCache cache)
+        public UserController(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher)
         {
             _commandDispatcher = commandDispatcher;
             _queryDispatcher = queryDispatcher;
-            _cache = cache;
         }
 
         [Authorize]
@@ -35,26 +31,6 @@ namespace YetAnotherTodoApp.Api.Controllers
             var userInfo = await _queryDispatcher.HandleAsync<GetUserInfoQuery, UserInfoDto>(new GetUserInfoQuery { UserId = userId });
 
             return Ok(userInfo);
-        }
-
-        [HttpPost]
-        public async Task RegisterUserAsync([FromBody] RegisterUserRequest request)
-        {
-            var command = new RegisterUserCommand(request.Username, request.Email, request.Password);
-            await _commandDispatcher.DispatchAsync(command);
-        }
-
-        [HttpPost("auth")]
-        public async Task<IActionResult> LoginUserAsync([FromBody] AuthenticateUserRequest request)
-        {
-            var command = new LoginUserCommand
-            {
-                TokenId = Guid.NewGuid(),
-                Email = request.Email,
-                Password = request.Password
-            };
-            await _commandDispatcher.DispatchAsync(command);
-            return Ok(_cache.Get(command.TokenId));
         }
 
         [Authorize]
