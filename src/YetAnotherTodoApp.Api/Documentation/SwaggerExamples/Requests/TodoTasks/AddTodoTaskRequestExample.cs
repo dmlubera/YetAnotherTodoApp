25f9cde1c00bd515aeee0b7c6d0060f11ce1c9e6
Damian Lubera <dmlubera@gmail.com>
2021-11-16 19:14:26 +0100

    Added missing swagger examples

---
+++ src/YetAnotherTodoApp.Api/Documentation/SwaggerExamples/Requests/TodoTasks/AddTodoTaskRequestExample.cs
@@ -0,0 +1,17 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+using System;
+using YetAnotherTodoApp.Api.Models.TodoTasks;
+
+namespace YetAnotherTodoApp.Api.Documentation.SwaggerExamples.Requests.TodoTasks
+{
+    public class AddTodoTaskRequestExample : IExamplesProvider<AddTodoTaskRequest>
+    {
+        public AddTodoTaskRequest GetExamples()
+            => new AddTodoTaskRequest
+            {
+                TodoId = Guid.Empty,
+                Title = "Something to do",
+                Description = "Not important"
+            };
+    }
+}
